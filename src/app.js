609f694db5d214df2067fcc37fa0d85cb31ca6ee
kristofwillen <kristof.willen@gmail.com>
2015-04-24 09:28:33 +0200

    version 0.2
    - show delay
    - fix minutes format to MM
    - add config option (doesn't work though)
    - add splash window while fetching data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 	/* Main app module */
  
 // VARS -----------------------------------------------------
-var UI = require('ui');
-var ajax = require('ajax');
+var UI     = require('ui');
+var ajax   = require('ajax');
+var Vector2 = require('vector2');
 var toStation;
 var fromStation;
+
+
+
+// CONFIGURATION ---------------------------------------------
+Pebble.addEventListener('showConfiguration', function(e) {
+  // Show config page
+  Pebble.openURL('http://kristof.willen.be/pebble/nmbs/config.html');
+});
+
+Pebble.addEventListener("webviewclosed",
+  function(e) {
+    //Get JSON dictionary
+    var configuration = JSON.parse(decodeURIComponent(e.response));
+    console.log("[DBUG] Configuration window returned: " + JSON.stringify(configuration));
+    //Pebble.sendAppMessage(configuration);
+    toStation   = configuration.toStation;
+    fromStation = configuration.fromStation;
+    console.log("[DBUG] We're going from " + fromStation + " to " + toStation);
+  }
+);
 
 
 // FUNCTIONS -------------------------------------------------
@@ -12,21 +33,28 @@ var fromStation;
 var parseFeed = function(data, quantity) {
   var items = [];
   for(var i = 0; i < quantity; i++) {
-    // Always upper case the description string
-    var dest = data.connection[i].departure.direction.name;
-    var gate = data.connection[i].departure.platform;
-    var epoch = data.connection[i].departure.time;
-    var travelTime = data.connection[i].duration/60;
-    var objDate = new Date(0);
+    var dest        = data.connection[i].departure.direction.name;
+    var gate        = data.connection[i].departure.platform;
+    var epoch       = data.connection[i].departure.time;
+    var travelTime  = data.connection[i].duration/60;
+    var delay       = data.connection[i].departure.delay/60;
+    var objDate     = new Date(0);
+    
+    // Change epoch return to HH:MM format
     objDate.setUTCSeconds(epoch);
-    var timestr = objDate.getHours() + ":" + objDate.getMinutes();
+    var minutes     = 0;
+    if (objDate.getMinutes() < 10) { minutes = "0" + objDate.getMinutes(); }
+    else { minutes  = objDate.getMinutes(); }
+    var timestr     = 0;
+    timestr = objDate.getHours() + ":" + minutes; 
+    if (delay > 0 ) { timestr = timestr + " [+" + delay + "m]"; }
     var travelHours = 0;
     while (travelTime >=60) {
-      travelHours += 1;
-      travelTime -= 60;
-    } 
-    
-   // Add to menu items array
+      travelHours  += 1;
+      travelTime   -= 60;
+    }
+        
+    // Add to menu items array
     items.push({
       title:timestr + " " + dest,
       subtitle:"spoor " + gate + " (" + travelHours + ":" + travelTime +")"
@@ -41,27 +69,46 @@ var parseFeed = function(data, quantity) {
 
 // MAIN ------------------------------------------------------
 // creating selection menu
+toStation   = "Leuven";
+fromStation = "Brussels-South";
+
+
 var typeMenu = new UI.Menu({
   sections: [{
     title: 'Select destination',
     items:[
-      {title:'Leuven'}, 
-      {title:'Brussels-South'}
+      {title:toStation}, 
+      {title:fromStation}
     ]
   }]
 });
 typeMenu.show();
 
-
 // on clicking "select" on menu generate get the list of trains 
 typeMenu.on('select', function(e) {
   toStation = e.item.title;
-  
-  // TODO make these configurable items
-  if (toStation == "Leuven") {  fromStation = "Brussels-South";}
+  if (toStation == "Leuven") { fromStation = "Brussels-South"; }
   else { fromStation = "Leuven"; }
-  
   var urlstr = "http://api.irail.be/connections/?to=" + toStation + "&from=" + fromStation + "&format=json";
+  
+  // Show splash screen while waiting for data
+  var splashWindow = new UI.Window();
+  // Text element to inform user
+  var text = new UI.Text({
+    position: new Vector2(0, 0),
+    size: new Vector2(144, 168),
+    text:'Downloading NMBS data...',
+    font:'GOTHIC_24_BOLD',
+    color:'black',
+    textOverflow:'wrap',
+    textAlign:'center',
+    backgroundColor:'white'
+  });
+  // Add to splashWindow and show
+  splashWindow.add(text);
+  splashWindow.show();
+  
+  console.log("[DBUG] Fetching " + urlstr);
   // Make request to irail.be
   ajax(
   {
@@ -79,10 +126,11 @@ typeMenu.on('select', function(e) {
       }]
     });
     resultsMenu.show();
+    splashWindow.hide();
   },
   
   function(error) {
-    console.log('Download failed: ' + error);
+    console.log('[FAIL] Download failed: ' + error);
     // Create the Card for error view
     var detailCard = new UI.Card({
       title:'Data fetch failed',
@@ -92,8 +140,6 @@ typeMenu.on('select', function(e) {
     detailCard.show();
   }
   );
- 
  }
 );
-
 
